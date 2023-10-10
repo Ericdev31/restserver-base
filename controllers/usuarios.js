2d359ae7779b585e7d5aea1f41232a1041497ffd
Eric Barrios García <ing.barriosgericabel@gmail.com>
2023-10-09 19:03:17 -0500

    restserver, peticio put en construcion

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,28 +1,91 @@
 const {response,request} = require('express');
+const bcryptjs = require('bcryptjs')
+const Usuario = require('../models/usuario');
 
 
-const usuariosGet = (req = request, res = response) => {
+ const usuariosGet = (req = request, res = response) => {
    const {q,nombre ='no name',apikey,page = 1,limit} = req.query;
     res.json({
+        msg: 'Get API - controlador',
         q,nombre,apikey,page,limit
     });
   }
 
- const usuariosPut =  (req, res) => {
-    const id = req.params.id;
+//  const usuariosPut = async(req, res = response) => {
+//     const {id} = req.params;
+//     const {password,google,correo, ...resto} = req.body;
+    
+//     //TODO:validar contra base de datos
 
-    res.status(500).json({
-        id,
-        msg: 'Put API - controlador'
-    })
+//     if (password) {
+//       const salt = bcryptjs.genSaltSync();
+//       resto.password = bcryptjs.hashSync(password, salt);
+//     }
+
+//     const usuario = await Usuario.findByIdAndUpdate(id,resto);
+
+//     res.json({
+//         msg: 'put API-usuarioPut',
+//         id
+//     })
+
+//  }
+
+
+const usuariosPut = async (req, res = response) => {
+  const { id } = req.params;
+  const { password, ...resto } = req.body;
+
+  try {
+      const usuario = await Usuario.findById(id);
+
+      if (!usuario) {
+          return res.status(404).json({
+              ok: false,
+              msg: 'Usuario no encontrado'
+          });
+      }
+
+      if (password) {
+          const salt = bcryptjs.genSaltSync();
+          resto.password = bcryptjs.hashSync(password, salt);
+      }
+
+      const usuarioActualizado = await Usuario.findByIdAndUpdate(id, resto, { new: true });
+
+      res.json({
+          ok: true,
+          usuario: usuarioActualizado
+      });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({
+          ok: false,
+          msg: 'Error al actualizar usuario'
+      });
   }
+}
 
-  const usuariosPost = (req, res) => {
-    const {nombre,edad} = req.body;
-    res.status(201).json({
-        msg: 'Post API - controlador ',
-        nombre,edad,
-        
+
+
+  const usuariosPost = async (req, res) => {
+     
+    // const {nombre,edad} = req.body;
+    const {nombre,correo,password, rol} = req.body;
+    const usuario = new Usuario({nombre,correo,password,rol});
+   
+    
+    //Encriptar la contraseña
+     const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password,salt);
+
+
+    //Guardar en DB
+    await usuario.save();
+  
+    res.json({
+        usuario
+       
     })
   }
 
@@ -33,7 +96,6 @@ const usuariosGet = (req = request, res = response) => {
 
     })
   }
-
 
  const usuauriosPatch =  (req, res) => {
     res.json({
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,17 +1,32 @@
 const {Router} = require('express');
+const { check } = require('express-validator');
+
+const{validarCampos} = require('../middlewares/validar-campos');
+const { esRoleValido, emailExiste, } = require('../helpers/db-validator');
+
 const { 
       usuariosGet, 
       usuariosPut,
       usuariosPost,
       usuariosDelete, 
       usuauriosPatch } = require('../controllers/usuarios');
+
 const router = Router();
 
 router.get('/', usuariosGet);
 
 router.put('/:id',usuariosPut);
 
-router.post('/', usuariosPost);
+router.post('/', [
+         check('nombre','El nombre es obligatorio').not().isEmpty(),
+         check('password','El password debe de traer mas de 6 letras').isLength({min: 6}),
+         check('correo','El correo no es valido').isEmail(),
+         check('correo').custom(emailExiste),
+         check('rol').custom( esRoleValido),
+      // check('rol','No es un rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+          
+      validarCampos
+],usuariosPost,);
 
 router.delete('/',usuariosDelete);
 
